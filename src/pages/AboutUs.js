44b1b4a91a75dc953a754cb2b5903433e45b4cc2
sherzodboy <kholmamatovsh09@gmail.com>
2023-05-21 13:53:44 +0500

    Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs = () => {
-  return <AboutSection />;
+  return (
+    <>
+      <AboutSection />
+      <ServicesSection />
+      <FaqSection />
+    </>
+  );
 };
 
 export default AboutUs;
