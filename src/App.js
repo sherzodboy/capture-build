ed7575c1546072140bc32f3125ff43390dbd827f
sherzodboy <kholmamatovsh09@gmail.com>
2023-05-21 15:13:07 +0500

    Styled Component to AboutSection

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import GlobalStyle from "./components/GlobalStyle";
+//import pages
 import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <AboutUs />
     </div>
   );
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,56 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #1b1b1b;
+
+  }
+
+  button {
+    font-weight: bold;
+    font-size: 1.1rem;
+    cursor: pointer;
+    padding: 1rem 2rem;
+    border: 3px solid #23d997;
+    background-color: transparent;
+    color: #FFFFFF;
+    transition: all 0.5s ease;
+    &:hover {
+      background-color: #23d997;
+      color: #FFFFFF;
+    }
+  }
+  h2 {
+    font-weight: lighter;
+    font-size: 4rem;
+  }
+  h3 {
+    color: #FFFFFF;
+  }
+  h4 {
+    font-weight: bold;
+  }
+  a {
+    font-size: 1.1rem;
+  }
+  span {
+    color: #23d997;
+    font-weight: bold;
+  }
+  p {
+    padding: 3rem 0rem;
+    color: gray;
+    font-size: 1.4rem;
+    line-height: 150%;
+  }
+
+`;
+
+export default GlobalStyle;
