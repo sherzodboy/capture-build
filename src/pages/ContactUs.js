3e7a44741e7ab0d0e4a34fc38f5361893fc4ddee
sherzodboy <kholmamatovsh09@gmail.com>
2023-05-28 20:21:30 +0500

    About Anim

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,10 +1,19 @@
 import React from "react";
+// Animation
+import { motion } from "framer-motion";
+import { pageAnimation } from "./../animation";
 
 const ContactUs = () => {
   return (
-    <div>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+      style={{ background: "#fff" }}
+    >
       <h1>ContactUs</h1>
-    </div>
+    </motion.div>
   );
 };
 
--- src/pages/MovieDetail.js
+++ src/pages/MovieDetail.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { useLocation } from "react-router-dom";
 import { MovieState } from "./../movieState";
+// Animation
+import { motion } from "framer-motion";
+import { pageAnimation } from "./../animation";
 
 const MovieDetail = () => {
   const location = useLocation();
@@ -17,7 +20,12 @@ const MovieDetail = () => {
   return (
     <>
       {movie && (
-        <Details>
+        <Details
+          exit="exit"
+          variants={pageAnimation}
+          initial="hidden"
+          animate="show"
+        >
           <HeadLine>
             <h2>{movie.title}</h2>
             <img src={movie.mainImg} alt="movie" />
@@ -40,7 +48,7 @@ const MovieDetail = () => {
   );
 };
 
-const Details = styled.div`
+const Details = styled(motion.div)`
   color: white;
 `;
 
